77c068b9c69329ee4cc5699620afbca1916bae4b
VANDINHHOAI <115123521+VANDINHHOAI@users.noreply.github.com>
2022-10-27 07:38:11 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,16 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1;
+		int b = 100;
+		int sum = 0;
+		for (int i = a; i <= b; i++) {
+			sum += i;
+		}
+		System.out.println("Tong cua 1 den 100 = " + sum);
+	}
+
+}
